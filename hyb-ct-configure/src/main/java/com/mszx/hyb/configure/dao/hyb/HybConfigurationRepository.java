92c4c6f0e4414c46a7995b6b21c5977a8210fbcc
王雷 <ahuiwanglei@126.com>
2021-05-03 19:18:12 +0800

    1

--- hyb-ct-configure/src/main/java/com/mszx/hyb/configure/dao/hyb/HybConfigurationRepository.java
+++ hyb-ct-configure/src/main/java/com/mszx/hyb/configure/dao/hyb/HybConfigurationRepository.java
@@ -3,6 +3,6 @@ package com.mszx.hyb.configure.dao.hyb;
 import com.mszx.hyb.configure.model.db.hyb.HybConfiguration;
 import com.mszx.hyb.configure.model.db.paycenter.PayMerchant;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//123123
 public interface HybConfigurationRepository extends JpaRepository<HybConfiguration, Integer> {
 }
